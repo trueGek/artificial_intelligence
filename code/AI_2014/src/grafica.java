21b5b600947d1cf9ebb5e5053cc158ca46843009
trueGek <truegek@Friko-Book.local>
2014-08-08 18:57:49 +0200

    Iniziati i primi lavori sul codice
    Iniziato a scrivere qualcosa dell’interfaccia

---
+++ code/AI_2014/src/grafica.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+class FrameP extends JFrame{
+	
+	JPanel c;
+	JLabel title;
+	JLabel intro;
+	
+	
+}
