cc02a7be601ff7e0f50706ce45ec5e63c1c55db2
trueGek <truegek@Friko-Book.local>
2014-08-19 18:35:13 +0200

    Aggiunte nuove maschere
    Creato nuove maschere per la selezione del nome utente in caso di
    giocatore umano

--- code/AI_2014/src/grafica.java
+++ code/AI_2014/src/grafica.java
@@ -15,9 +15,15 @@ class FrameP extends JFrame{
 	JRadioButton game_1 = new JRadioButton("CPU VS umano");
 	JRadioButton game_2 = new JRadioButton("CPU VS CPU");
 	ButtonGroup group = new ButtonGroup();
+	JButton cont = new JButton("Continua");
 	JButton start = new JButton("Inizia gioco");
 	JButton close = new JButton("Chiudi");
 
+/*Inserimento nome*/
+	String user_1 = "CPU 1";
+	String user_2 = "CPU 2";
+	JTextField your_name;
+	
 /*Structure*/
 	JPanel titles = new JPanel();
 	JPanel ts_buttons1 = new JPanel();
@@ -48,7 +54,7 @@ class FrameP extends JFrame{
 		group.add(game_2);
 		
 		titles.setLayout(new BorderLayout());
-		titles.setSize(600,400);
+		titles.setSize(600,200);
 		titles.add(title, BorderLayout.NORTH);
 		
 		ts_buttons1.setLayout(new BorderLayout());
@@ -73,14 +79,93 @@ class FrameP extends JFrame{
 	}
 	
 	
+	private void InsertName() {
+				
+		c.removeAll();
+		
+		c = (JPanel)this.getContentPane();
+		this.setSize(300,200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		c.setLayout(new BoxLayout(c, BoxLayout.Y_AXIS));
+		
+		user_2 = "Giocatore umano";
+		your_name = new JTextField(user_2,15);
+		
+		JLabel desc_name = new JLabel("Inserisci il tuo nome:");
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setFont(new Font("Serif", Font.PLAIN, 25));
+		desc_name.setHorizontalAlignment(SwingConstants.CENTER);
+		desc_name.setFont(new Font("Serif", Font.PLAIN, 18));
+		 
+		your_name.setHorizontalAlignment(SwingConstants.CENTER);
+		your_name.setFont(new Font("Serif", Font.PLAIN, 18));
+		
+		cont.addActionListener(new PlayGame());
+		
+		JPanel field = new JPanel();
+		field.setLayout(new BorderLayout());
+		
+		field.add(your_name, BorderLayout.NORTH);		
+		c.add(title, c);
+		c.add(Box.createRigidArea(new Dimension(0,10)));
+		c.add(desc_name, c);
+		c.add(Box.createRigidArea(new Dimension(0,10)));
+		c.add(field, c);
+		c.add(Box.createRigidArea(new Dimension(0,10)));
+		c.add(cont, c);
+		
+	}
+	
+	private void GameTable(){
+		
+		c.removeAll();
+		c = (JPanel)this.getContentPane();
+		this.setSize(300,201);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		c.setLayout(new BoxLayout(c, BoxLayout.Y_AXIS));
+		
+		JLabel index = new JLabel(user_1 + " VS " + user_2);
+		
+		c.add(index, c);
+		
+		
+		
+	}
+	
 	class StartGame implements ActionListener {
 		
 		public void actionPerformed(ActionEvent e){
 			
 			JButton button = (JButton)e.getSource();
 			if(button.getText()=="Inizia gioco"){
-				String name = JOptionPane.showInputDialog(null,"Come ti chiami?", null);
-
+				if(game_1.isSelected()){
+					InsertName();
+					start.removeActionListener(this);
+				}else{
+					
+					if(game_2.isSelected()){
+						GameTable();
+						start.removeActionListener(this);
+						
+					}
+					
+				}
+			}
+			
+		}
+		
+	}
+	
+	class PlayGame implements ActionListener{
+		
+		public void actionPerformed(ActionEvent e){
+			
+			JButton button = (JButton)e.getSource();
+			if(button.getText()=="Continua"){
+				
+				user_2 = your_name.getText();
+				GameTable();
+				cont.removeActionListener(this);
 			}
 			
 		}
